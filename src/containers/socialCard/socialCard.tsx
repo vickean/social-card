a9e27ab43cee31ed9a136f36d442ca29fbf3c48f
vickean <vickean@gmail.com>
2020-03-28 20:03:44 +0800

    dinner break

---
+++ src/containers/socialCard/socialCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const SocialCard = (props: any) => {
+    return (
+        <div>
+            <h1>
+                Hello world!
+                This is a new React App.
+            </h1>
+        </div>
+    )
+} 
